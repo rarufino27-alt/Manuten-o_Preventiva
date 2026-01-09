9fc04cced2463dcbde18e82a4835cc39dbf4691b
rarufino27-alt <rarufino27@gmail.com>
2026-01-08 23:07:23 -0300

    Update supabase.js

--- core/supabase.js
+++ core/supabase.js
@@ -4,7 +4,7 @@
 
 // 🔒 SUPABASE FIXO (RF DRIVER)
 const SUPABASE_URL = "https://msnqiiwcityikslikbow.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zbnFpaXdjaXR5aWtzbGlrYm93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc3MDM5OTgsImV4cCI6MjA4MzI3OTk5OH0.0KyaSuL5At4_Cfa4TOM7kvvkVYv-gmR2sb7vX6VHkaU";
 
 // 🧠 GRUPO FIXO (RF Driver)
 const GRUPO_ID_FIXO = "f5b2eec4-f957-4648-8e4e-f5642159dd8c";
